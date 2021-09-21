464f8e51e3625d661894370daf592e8fef4996e3
danielphan2003 <danielphan2003@users.noreply.github.com>
2021-09-21 22:17:18 +0000

    pkgs: auto update on 2021/09/21 @ 22:17:17 discord-canary: 0.0.130 → 0.0.131

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -175,10 +175,10 @@
   };
   discord-canary = {
     pname = "discord-canary";
-    version = "0.0.130";
+    version = "0.0.131";
     src = fetchurl {
-      url = "https://dl-canary.discordapp.net/apps/linux/0.0.130/discord-canary-0.0.130.tar.gz";
-      sha256 = "1razxxx16bjzhv9909qa08dmlmgg7ji1vckvkggw7syi125r5aai";
+      url = "https://dl-canary.discordapp.net/apps/linux/0.0.131/discord-canary-0.0.131.tar.gz";
+      sha256 = "087rzyivk0grhc73v7ldxxghks0n16ifrvpmk95vzaw99l9xv0v5";
     };
   };
   dnscrypt-proxy2 = {
