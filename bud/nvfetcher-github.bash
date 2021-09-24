66613b9dc3ab60984062ef28f7c96aa29b683cf1
Daniel Phan <danielphan.2003@gmail.com>
2021-09-24 13:10:39 +0700

    spotify: handle auto update

--- bud/nvfetcher-github.bash
+++ bud/nvfetcher-github.bash
@@ -1,4 +1,4 @@
-cd $FLAKEROOT/pkgs
+cd "$FLAKEROOT"/pkgs || exit
 
 # remove old Cargo.lock
 fd . _sources \
@@ -7,13 +7,27 @@ fd . _sources \
   -E '.shake.*' \
   -X rm -rf {}
 
+SPOTIFY_INFO="$(curl -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/spotify?channel=candidate')"
+SPOTIFY_SRC_MANUAL="$(echo "$SPOTIFY_INFO" | jq -r '.version')"
+SPOTIFY_FETCH_URL="$(echo "$SPOTIFY_INFO" | jq -r '.download_url')"
+
+OLD_SPOTIFY_SRC_MANUAL="$(dasel select -f ./sources.toml -p toml '.spotify.src.manual')"
+OLD_SPOTIFY_FETCH_URL="$(dasel select -f ./sources.toml -p toml '.spotify.fetch.url')"
+
+sed -i \
+  -e "s#$OLD_SPOTIFY_SRC_MANUAL#$SPOTIFY_SRC_MANUAL#g" \
+  -e "s#$OLD_SPOTIFY_FETCH_URL#$SPOTIFY_FETCH_URL#g" \
+  ./sources.toml
+
 nvfetcher -c ./sources.toml -l changelog --no-output
 
 if [ ! -z "$(cat changelog)" ]; then
-  echo "COMMIT_MSG<<EOF" >>$GITHUB_ENV
-  echo "pkgs: auto update on $(date +"%Y/%m/%d @ %H:%M:%S")" >>$GITHUB_ENV
-  echo "$(cat changelog)" >>$GITHUB_ENV
-  echo "EOF" >>$GITHUB_ENV
+  {
+    echo "COMMIT_MSG<<EOF"
+    echo "pkgs: auto update on $(date +"%Y/%m/%d @ %H:%M:%S")"
+    cat changelog
+    echo "EOF"
+  } >> "$GITHUB_ENV"
   rm -rf changelog
 else
   rm -rf changelog
