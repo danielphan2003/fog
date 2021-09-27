dc5c31ef1a05ba2277c2f9fb0b511ad24a92f9d3
Daniel Phan <danielphan.2003@gmail.com>
2021-09-27 12:53:42 +0700

    bud: clean up deps

--- bud/default.nix
+++ bud/default.nix
@@ -1,16 +1,7 @@
-{ pkgs, lib, budUtils, ... }:
-let
-  writeBashWithNixPaths = paths: name: script:
-    pkgs.writers.writeBash name ''
-      export PATH="${lib.makeBinPath paths}"
-      export NIX_PATH="$NIX_PATH:nixpkgs=${toString pkgs.path}"
-      source ${script}
-    '';
-in
-{
+{ pkgs, budUtils, ... }: {
   bud.cmds = with pkgs; {
     nvfetcher-cleanup = {
-      writer = writeBashWithNixPaths [ coreutils fd dasel curl jq gnused ];
+      writer = budUtils.writeBashWithPaths [ coreutils fd dasel curl gnused ];
       synopsis = "nvfetcher-cleanup";
       help = "Clean up nvfetcher on github action";
       script = ./nvfetcher-cleanup.bash;
--- bud/nvfetcher-cleanup.bash
+++ bud/nvfetcher-cleanup.bash
@@ -7,12 +7,12 @@ fd . _sources \
   -E '.shake.*' \
   -X rm -rf {}
 
-SPOTIFY_INFO="$(curl -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/spotify?channel=candidate')"
-SPOTIFY_SRC_MANUAL="$(echo "$SPOTIFY_INFO" | jq -r '.version')"
-SPOTIFY_FETCH_URL="$(echo "$SPOTIFY_INFO" | jq -r '.download_url')"
+SPOTIFY_INFO="$(curl -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/spotify?channel=candidate' | tr '\\\n' ' ')"
+SPOTIFY_SRC_MANUAL="$(echo "$SPOTIFY_INFO" | dasel -p json --plain '.version')"
+SPOTIFY_FETCH_URL="$(echo "$SPOTIFY_INFO" | dasel -p json --plain '.download_url')"
 
-OLD_SPOTIFY_SRC_MANUAL="$(dasel select -f ./sources.toml -p toml '.spotify.src.manual')"
-OLD_SPOTIFY_FETCH_URL="$(dasel select -f ./sources.toml -p toml '.spotify.fetch.url')"
+OLD_SPOTIFY_SRC_MANUAL="$(dasel -f ./sources.toml -p toml '.spotify.src.manual')"
+OLD_SPOTIFY_FETCH_URL="$(dasel -f ./sources.toml -p toml '.spotify.fetch.url')"
 
 sed -i \
   -e "s#$OLD_SPOTIFY_SRC_MANUAL#$SPOTIFY_SRC_MANUAL#g" \
