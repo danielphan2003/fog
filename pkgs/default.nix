39cd9a7e13c0fda23a8f1bc656fb7a284b2a18eb
Daniel Phan <danielphan.2003@gmail.com>
2021-10-25 01:51:28 +0700

    pkgs: add fetchFromGitHub

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
 final: prev:
 let
   inherit (prev.lib) mapAttrs removePrefix;
-  sources' = import ./_sources/generated.nix { inherit (final) fetchurl fetchgit; };
+  sources' = import ./_sources/generated.nix { inherit (final) fetchgit fetchurl fetchFromGitHub; };
   sources =
     mapAttrs
       (pname: meta: meta // { version = removePrefix "v" meta.version; })
