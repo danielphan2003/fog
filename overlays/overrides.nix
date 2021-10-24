2e8f357a0f4e0ff98d308490ba258430c3c64142
Daniel Phan <danielphan.2003@gmail.com>
2021-10-25 01:02:43 +0700

    overrides: remove haskell ref

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -8,16 +8,4 @@ channels: final: prev: {
     nvchecker
     ;
 
-  haskellPackages = prev.haskellPackages.override
-    (old: {
-      overrides = prev.lib.composeExtensions (old.overrides or (_: _: { })) (hfinal: hprev:
-        let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-        in
-        {
-          # same for haskell packages, matching ghc versions
-          inherit (channels.latest.haskell.packages."ghc${version}")
-            haskell-language-server;
-        });
-    });
-
 }
