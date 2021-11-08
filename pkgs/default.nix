45ca0049c1950ca90887c5607b6d7cc5131b1ca8
Daniel Phan <danielphan.2003@gmail.com>
2021-11-08 09:29:23 +0700

    pkgs: remove passthru hack

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,5 @@
 final: prev: {
-  # a hacky way so that we can run ` nix eval --raw '.#sources.${pname}.src' `
-  sources = final.emptyFile.overrideAttrs (_: {
-    passthru = final.callPackage ./_sources/generated.nix { };
-  });
-
-  # this doesn't work
-  # sources = (final.callPackage ./_sources/generated.nix { }) // {
-  #   __dontExport = false;
-  # };
+  # run ` nix-instantiate --eval -E $(n eval --raw .\#pkgs.$(uname -m)-linux.latest.${pname}.src) `
+  # assuming that we are running on linux
+  sources = final.callPackage ./_sources/generated.nix { };
 }
