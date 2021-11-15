4764ba25f7b26db70c7bd5bd58b81c859646be81
GitHub automation <danielphan.2003@gmail.com>
2021-11-15 12:36:13 +0000

    vscode-extensions-svelte: 105.5.0 → 105.5.2

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -1477,10 +1477,10 @@
   };
   vscode-extensions-svelte = {
     pname = "vscode-extensions-svelte";
-    version = "105.5.0";
+    version = "105.5.2";
     src = fetchurl {
-      url = "https://open-vsx.org/api/svelte/svelte-vscode/105.5.0/file/svelte.svelte-vscode-105.5.0.vsix";
-      sha256 = "sha256-JTbscWRHN4OZ5iBpVYbfZF94cW0F502MuAYegRi7jr8=";
+      url = "https://open-vsx.org/api/svelte/svelte-vscode/105.5.2/file/svelte.svelte-vscode-105.5.2.vsix";
+      sha256 = "sha256-059AtF+F0Ijg29aqvgszo0WpKt6fzgE94sIYcYMh0R0=";
     };
     homepage = "https://github.com/sveltejs/language-tools";
     license = "mit";
