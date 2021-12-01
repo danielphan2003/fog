7eaac3274ddfaa8ac51d3ff5dea2ab6ec7665a2c
Daniel Phan <danielphan.2003@gmail.com>
2021-12-01 16:02:45 +0700

    nixpkgs: bump release-21.11

--- flake.nix
+++ flake.nix
@@ -2,23 +2,20 @@
   description = "A highly structured configuration database.";
 
   nixConfig = {
-    extra-experimental-features = "nix-command flakes ca-references";
+    extra-experimental-features = "nix-command flakes";
     extra-substituters = "https://cache.nixos.org https://nrdxp.cachix.org https://nix-community.cachix.org https://dan-cfg.cachix.org";
     extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= dan-cfg.cachix.org-1:elcVKJWjnDs1zzZ/Fs93FLOFS13OQx1z0TxP0Q7PH9o=";
   };
 
   inputs = {
-    nixos.url = "nixpkgs/release-21.05";
+    nixos.url = "nixpkgs/release-21.11";
     latest.url = "nixpkgs/nixos-unstable";
 
-    nix = { url = github:nixos/nix/2.4pre-rc1; };
-
     digga = {
-      url = github:divnix/digga;
+      url = github:divnix/digga/cleanup-dar;
       inputs = {
-        nix.follows = "nix";
         nixpkgs.follows = "nixos";
-        latest.follows = "latest";
+        latest.follows = "nixos";
         nixlib.follows = "nixos";
       };
     };
@@ -34,7 +31,7 @@
     nvfetcher = {
       url = github:berberman/nvfetcher;
       inputs = {
-        nixpkgs.follows = "latest";
+        nixpkgs.follows = "nixos";
         flake-compat.follows = "digga/deploy/flake-compat";
         flake-utils.follows = "digga/flake-utils-plus/flake-utils";
       };
@@ -58,13 +55,11 @@
           nixos = {
             imports = [ (digga.lib.importOverlays ./overlays) ];
             overlays = [
-              digga.overlays.patchedNix
               nvfetcher.overlay
+              ./pkgs/default.nix
             ];
           };
-          latest = {
-            overlays = [ ./pkgs/default.nix ];
-          };
+          latest = { };
         };
 
         nixos.hostDefaults.channelName = "nixos";
