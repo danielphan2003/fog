5dfb777f25efde0de61ac4f08ec3d44a6ba614cc
Daniel Phan <34669737+danielphan2003@users.noreply.github.com>
2021-12-30 05:31:09 +0700

    bud: add gnupg for updateSources

--- bud/default.nix
+++ bud/default.nix
@@ -38,7 +38,7 @@ in
       script = ./utils/patch-sources.bash;
     };
     updateSources = {
-      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git nix-prefetch gnutar xz git bash ];
+      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git nix-prefetch gnutar xz git bash gnupg ];
       synopsis = "updateSources";
       help = "Update source";
       script = ./utils/update-sources.bash;
