0b387befe0e10f7c64e208b5acebb886dea8e983
Daniel Phan <34669737+danielphan2003@users.noreply.github.com>
2021-12-30 04:37:20 +0700

    bud: add xz for updateSources

--- bud/default.nix
+++ bud/default.nix
@@ -38,7 +38,7 @@ in
       script = ./utils/patch-sources.bash;
     };
     updateSources = {
-      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git nix-prefetch gnutar ];
+      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git nix-prefetch gnutar xz ];
       synopsis = "updateSources";
       help = "Update source";
       script = ./utils/update-sources.bash;
