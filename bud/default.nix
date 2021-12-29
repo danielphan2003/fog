02f8a64fd843e9d4f593e075e92c72f129947ad7
Daniel Phan <danielphan.2003@gmail.com>
2021-12-30 03:49:37 +0700

    bud: add nix-prefetch for updateSources

--- bud/default.nix
+++ bud/default.nix
@@ -38,7 +38,7 @@ in
       script = ./utils/patch-sources.bash;
     };
     updateSources = {
-      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git ];
+      writer = writeBashWithBudPaths [ coreutils nvfetcher-bin git nix-prefetch ];
       synopsis = "updateSources";
       help = "Update source";
       script = ./utils/update-sources.bash;
