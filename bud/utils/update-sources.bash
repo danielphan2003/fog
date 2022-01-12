55fe8c2a6180a9264e2179ee79c58f52d4867a96
Daniel Phan <danielphan.2003@gmail.com>
2022-01-12 11:24:49 +0700

    open-vsx: fix up for matklad.rust-analyzer rule

--- bud/utils/update-sources.bash
+++ bud/utils/update-sources.bash
@@ -2,10 +2,12 @@ file="$1"
 sourceDir="_sources/${2:-"$(basename $file)"}"
 file="$file.toml"
 
-args=( "${@: 3}" )
+args=( "${@: 2}" )
 if [ $CI ]; then
   args+=( --commit-changes )
 fi
+
+echo "${args[@]}"
 
 cd "$PRJ_ROOT"/pkgs || exit
 
