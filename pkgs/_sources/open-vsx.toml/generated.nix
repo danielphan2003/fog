e0ac75751f6bac2bece0c34a7993cd56ea460e0b
GitHub automation <danielphan.2003@gmail.com>
2022-02-04 03:25:47 +0000

    mintlify-document: ∅ → 1.8.1

--- pkgs/_sources/open-vsx.toml/generated.nix
+++ pkgs/_sources/open-vsx.toml/generated.nix
@@ -11115,10 +11115,10 @@
   };
   mintlify-document = {
     pname = "mintlify-document";
-    version = "1.8.0";
+    version = "1.8.1";
     src = fetchurl {
-      url = "https://open-vsx.org/api/mintlify/document/1.8.0/file/mintlify.document-1.8.0.vsix";
-      sha256 = "sha256-t0OQEemggKmNozZwH+9mbpe1i0Sp5eFyK6kb356qFDI=";
+      url = "https://open-vsx.org/api/mintlify/document/1.8.1/file/mintlify.document-1.8.1.vsix";
+      sha256 = "sha256-k8U1ZM/AjSQvcMjH/94ViPEewsRhyPCzBwIQuV9pf6Y=";
     };
     name = "document";
     license = "Apache-2.0";
