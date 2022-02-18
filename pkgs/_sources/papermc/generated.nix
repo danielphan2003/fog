3dc9e303c873a10e87fc0c9815d307e7776947fb
GitHub automation <danielphan.2003@gmail.com>
2022-02-18 03:21:34 +0000

    papermc-1_18_1: 199 → 200

--- pkgs/_sources/papermc/generated.nix
+++ pkgs/_sources/papermc/generated.nix
@@ -228,10 +228,10 @@
   };
   papermc-1_18_1 = {
     pname = "papermc-1_18_1";
-    version = "199";
+    version = "200";
     src = fetchurl {
-      url = "https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/199/downloads/paper-1.18.1-199.jar";
-      sha256 = "sha256-DE8Ys9jwfvzYxaUfO/0AF9oSdMsJMkQx4yYzELAvJEc=";
+      url = "https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/200/downloads/paper-1.18.1-200.jar";
+      sha256 = "sha256-VDVzjemlM7HP/Y7HOCHQvoUS7fUccKEbiWNqWgCJlME=";
     };
     mcVer = "1.18.1";
   };
