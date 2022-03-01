604da4005f315ff75d37e7c29183463bf8374216
GitHub automation <danielphan.2003@gmail.com>
2022-03-01 13:35:20 +0000

    bilelmoussaoui-flatpak-vscode: 0.0.21 → 0.0.22

--- pkgs/_sources/open-vsx/generated.nix
+++ pkgs/_sources/open-vsx/generated.nix
@@ -5151,10 +5151,10 @@
   };
   bilelmoussaoui-flatpak-vscode = {
     pname = "bilelmoussaoui-flatpak-vscode";
-    version = "0.0.21";
+    version = "0.0.22";
     src = fetchurl {
-      url = "https://open-vsx.org/api/bilelmoussaoui/flatpak-vscode/0.0.21/file/bilelmoussaoui.flatpak-vscode-0.0.21.vsix";
-      sha256 = "sha256-Zkc72Wh7WNcm3hmuvM5vnp85dZfbZGuTVX9WkAzsVUY=";
+      url = "https://open-vsx.org/api/bilelmoussaoui/flatpak-vscode/0.0.22/file/bilelmoussaoui.flatpak-vscode-0.0.22.vsix";
+      sha256 = "sha256-nRm8NEje0PFwME/ZFYfhQ8L6LPPe08S4wFxbiqZz1e4=";
     };
     name = "flatpak-vscode";
     license = "MIT";
