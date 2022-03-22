fff08aaab6ab09acaf0fdd22e257a5e0329363b7
GitHub automation <danielphan.2003@gmail.com>
2022-03-22 07:14:22 +0000

    wcrichton-flowistry: 0.5.12 → 0.5.14

--- pkgs/_sources/open-vsx/generated.nix
+++ pkgs/_sources/open-vsx/generated.nix
@@ -18915,10 +18915,10 @@
   };
   wcrichton-flowistry = {
     pname = "wcrichton-flowistry";
-    version = "0.5.12";
+    version = "0.5.14";
     src = fetchurl {
-      url = "https://open-vsx.org/api/wcrichton/flowistry/0.5.12/file/wcrichton.flowistry-0.5.12.vsix";
-      sha256 = "sha256-MZWMHx6NlpHLJaXrkq8d7IFwfycOc19cLj8OuDPuIM8=";
+      url = "https://open-vsx.org/api/wcrichton/flowistry/0.5.14/file/wcrichton.flowistry-0.5.14.vsix";
+      sha256 = "sha256-RtXIaRaqSTzAMKo28Qy+lRwXadnwZW2VC6W7UJZdblA=";
     };
     name = "flowistry";
     license = "MIT";
