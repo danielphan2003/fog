516b405ac17ae47956bd8adc90871511b898e920
GitHub automation <danielphan.2003@gmail.com>
2022-03-29 03:52:43 +0000

    engn2219-anu-engn2219-2022-s1-vscode: 1.0.0 → 1.1.0

--- pkgs/_sources/open-vsx/generated.nix
+++ pkgs/_sources/open-vsx/generated.nix
@@ -7431,10 +7431,10 @@
   };
   engn2219-anu-engn2219-2022-s1-vscode = {
     pname = "engn2219-anu-engn2219-2022-s1-vscode";
-    version = "1.0.0";
+    version = "1.1.0";
     src = fetchurl {
-      url = "https://open-vsx.org/api/engn2219-anu/engn2219-2022-s1-vscode/1.0.0/file/engn2219-anu.engn2219-2022-s1-vscode-1.0.0.vsix";
-      sha256 = "sha256-1dTZKkpKIwg7PatnQDcEEBD2eZb2Xprf9YSOcrpMkS8=";
+      url = "https://open-vsx.org/api/engn2219-anu/engn2219-2022-s1-vscode/1.1.0/file/engn2219-anu.engn2219-2022-s1-vscode-1.1.0.vsix";
+      sha256 = "sha256-s7xweVNJFcD6hBvXS76HQNRO5cJJeLZqHsfbUCVtM78=";
     };
     name = "engn2219-2022-s1-vscode";
     license = "MIT";
