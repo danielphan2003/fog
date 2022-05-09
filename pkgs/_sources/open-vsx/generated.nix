3d3f5ff4b06cdac2bfc4ba287ba69549fad415e3
GitHub automation <danielphan.2003@gmail.com>
2022-05-09 19:33:58 +0000

    Prisma-prisma-insider: 31.0.945 → 31.0.946

--- pkgs/_sources/open-vsx/generated.nix
+++ pkgs/_sources/open-vsx/generated.nix
@@ -2775,10 +2775,10 @@
   };
   Prisma-prisma-insider = {
     pname = "Prisma-prisma-insider";
-    version = "31.0.945";
+    version = "31.0.946";
     src = fetchurl {
-      url = "https://open-vsx.org/api/Prisma/prisma-insider/31.0.945/file/Prisma.prisma-insider-31.0.945.vsix";
-      sha256 = "sha256-Jc24kDGBIJvcDATN7hFF8n5iPlVFsyj2rVPdFYR2nzk=";
+      url = "https://open-vsx.org/api/Prisma/prisma-insider/31.0.946/file/Prisma.prisma-insider-31.0.946.vsix";
+      sha256 = "sha256-HrM/d68I7tOZVd+0xWG3KpMj9EwYpRcX0u+cJgSa5uc=";
     };
     name = "prisma-insider";
     license = "Apache-2.0";
