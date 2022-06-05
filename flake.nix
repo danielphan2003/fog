9a12f00b098e13c3dd30865ebb09606f33cb54f7
Daniel Phan <danielphan.2003@c-137.me>
2022-06-06 02:08:18 +0700

    nixpkgs: remove commit-changes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
   description = "An opinionated way of updating nixpkgs source.";
 
-  nixConfig = {
-    extra-experimental-features = "nix-command flakes";
-    extra-substituters = "https://cache.nixos.org https://nrdxp.cachix.org https://nix-community.cachix.org https://dan-cfg.cachix.org";
-    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= dan-cfg.cachix.org-1:elcVKJWjnDs1zzZ/Fs93FLOFS13OQx1z0TxP0Q7PH9o=";
-  };
-
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.std.url = "github:divnix/std";
   inputs.std.inputs.nixpkgs.follows = "nixpkgs";
@@ -37,4 +31,22 @@
       ];
     }
   ;
+
+  # --- Flake Local Nix Configuration ----------------------------
+  # TODO: adopt spongix
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nrdxp.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://dan-cfg.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "dan-cfg.cachix.org-1:elcVKJWjnDs1zzZ/Fs93FLOFS13OQx1z0TxP0Q7PH9o="
+    ];
+  };
+  # --------------------------------------------------------------
 }
