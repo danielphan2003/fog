f543abe23b04c7c25d1471a10bf2f7a61c7e5b06
GitHub automation <danielphan.2003@gmail.com>
2022-06-05 20:06:52 +0000

    Update: misc/minecraft-mods

--- src/minecraft-mods/generated.nix
+++ src/minecraft-mods/generated.nix
@@ -8,8 +8,8 @@
       url = "https://cdn.modrinth.com/data/kKwy3HU9/versions/1.1.0/betterbeds-1.1.0.jar";
       sha256 = "sha256-cHZpq8SS3ffruSQ7Jn07mVBLGRb8zNz6qTj+WGitVds=";
     };
-    modId = "kKwy3HU9";
     mcVer = "1.16";
+    modId = "kKwy3HU9";
   };
   better-beds-1_17 = {
     pname = "better-beds-1_17";
@@ -18,8 +18,8 @@
       url = "https://cdn.modrinth.com/data/kKwy3HU9/versions/1.2.0/betterbeds-1.2.0.jar";
       sha256 = "sha256-ePkc4oTD0SGFD7qnzKSgHrW9fPoDMpni8euR8aowPA8=";
     };
-    modId = "kKwy3HU9";
     mcVer = "1.16";
+    modId = "kKwy3HU9";
   };
   cull-leaves-1_16 = {
     pname = "cull-leaves-1_16";
@@ -28,8 +28,8 @@
       url = "https://cdn.modrinth.com/data/GNxdLCoP/versions/2.0.0/cullleaves-2.0.0.jar";
       sha256 = "sha256-1vqwrorAl8YSNBxDnQOWK4tZb5LEaeCymyUGUtDLs4U=";
     };
-    modId = "GNxdLCoP";
     mcVer = "1.16";
+    modId = "GNxdLCoP";
   };
   cull-leaves-1_16_1 = {
     pname = "cull-leaves-1_16_1";
@@ -38,8 +38,8 @@
       url = "https://cdn.modrinth.com/data/GNxdLCoP/versions/2.1.0/cullleaves-2.1.0.jar";
       sha256 = "sha256-x6GwaiYH9GetsM4tdw8lIuoI0/ls2MACXrNA27HT2BM=";
     };
-    modId = "GNxdLCoP";
     mcVer = "1.16.1";
+    modId = "GNxdLCoP";
   };
   cull-leaves-1_17 = {
     pname = "cull-leaves-1_17";
@@ -48,8 +48,8 @@
       url = "https://cdn.modrinth.com/data/GNxdLCoP/versions/2.3.2/cullleaves-2.3.2.jar";
       sha256 = "sha256-tNi43VJa/tLpKcsSEhIjYim6pYopTlBRCy3zdtNC6xQ=";
     };
-    modId = "GNxdLCoP";
     mcVer = "1.17";
+    modId = "GNxdLCoP";
   };
   fabric-api-1_14 = {
     pname = "fabric-api-1_14";
@@ -58,8 +58,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.2.7+build.127/fabric-0.2.7+build.127.jar";
       sha256 = "sha256-Uv//C6GCzlwB9nVHSd+QcT3w272pHoPKBdevrdLvnpg=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.14";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_14_1 = {
     pname = "fabric-api-1_14_1";
@@ -68,8 +68,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.3.0-pre+build.157a/fabric-api-0.3.0-pre+build.157a.jar";
       sha256 = "sha256-L7y7Lj7izi3nEwjG1tJVziOirIgay8kwqDamgYkN/50=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.14.1";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_14_2 = {
     pname = "fabric-api-1_14_2";
@@ -78,8 +78,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.3.0+build.185/fabric-api-0.3.0+build.185.jar";
       sha256 = "sha256-V5YjA7QYTOUAwuyQuFyFtJXV0L35wvD4Bv/IKBVXkyg=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.14.2";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_14_3 = {
     pname = "fabric-api-1_14_3";
@@ -88,8 +88,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.3.0+build.198/fabric-api-0.3.0+build.198.jar";
       sha256 = "sha256-p7AQHjJxLhIohG/jJMawwDfuBREJFk0K2iup2Z+Mowc=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.14.3";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_14_4 = {
     pname = "fabric-api-1_14_4";
@@ -98,8 +98,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.28.5+1.14/fabric-api-0.28.5+1.14.jar";
       sha256 = "sha256-9emDhUvdp3Q2KCMc9I7APZBD7il+TCAVh4ALkWP4sK0=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.14.4";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_15 = {
     pname = "fabric-api-1_15";
@@ -108,8 +108,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.4.24+build.279-1.15/fabric-api-0.4.24+build.279-1.15.jar";
       sha256 = "sha256-x6Xe5JD8VdWoieJoHz1J0N/sNtyqPC34MR/0jkyJDBY=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.15";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_15_1 = {
     pname = "fabric-api-1_15_1";
@@ -118,8 +118,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.4.27+build.286-1.15/fabric-api-0.4.27+build.286-1.15.jar";
       sha256 = "sha256-BDrxap1aEsbQRTxRSYEBvmYTYxwiPXwvR6lmqLIiSf4=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.1";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_15_2 = {
     pname = "fabric-api-1_15_2";
@@ -128,8 +128,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.28.5+1.15/fabric-api-0.28.5+1.15.jar";
       sha256 = "sha256-lnC0VgfSlEoeSOSSw7QPI9WshxlB8I19AfHK2g53wKo=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.15.2";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16 = {
     pname = "fabric-api-1_16";
@@ -138,8 +138,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.13.1+build.370-1.16/fabric-api-0.13.1+build.370-1.16.jar";
       sha256 = "sha256-sVrBUgP+F7/aHeL1vL6zNJ5LedMluhtv/LF94A82+ZI=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16_1 = {
     pname = "fabric-api-1_16_1";
@@ -148,8 +148,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.18.0+build.387-1.16.1/fabric-api-0.18.0+build.387-1.16.1.jar";
       sha256 = "sha256-7SdIJd07EGoN4EtfxGhpJ7QBig3khIT1b2TId5HtTbs=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.1";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16_2 = {
     pname = "fabric-api-1_16_2";
@@ -158,8 +158,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.20.1+build.401-1.16/fabric-api-0.20.1+build.401-1.16.jar";
       sha256 = "sha256-+z9AlmYmqG7EAQzuxHFAqd7FLEaMTDJEQQkbAk4cu8o=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.2";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16_3 = {
     pname = "fabric-api-1_16_3";
@@ -168,8 +168,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.25.0+build.415-1.16/fabric-api-0.25.0+build.415-1.16.jar";
       sha256 = "sha256-5+oIAqKmB/S7OwiddYHyV0fjgurwfLearibtXE37x1I=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.3";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16_4 = {
     pname = "fabric-api-1_16_4";
@@ -178,8 +178,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.29.3+1.16/fabric-api-0.29.3+1.16.jar";
       sha256 = "sha256-PbzOgbUZF9GewfyVyFcWMEJ58XkIqa4t76+g8/+t8Q0=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.4";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_16_5 = {
     pname = "fabric-api-1_16_5";
@@ -188,8 +188,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.42.0+1.16/fabric-api-0.42.0+1.16.jar";
       sha256 = "sha256-PfjdUD81qgrJ+ritn5o2n9/QsatUSvGaPWJtlI+0WGw=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.16.5";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_17 = {
     pname = "fabric-api-1_17";
@@ -198,8 +198,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.36.0+1.17/fabric-api-0.36.0+1.17.jar";
       sha256 = "sha256-hub8Ijb522iIFeASD2Hppa3mrqjDy8qwQv6rvt3EsTM=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.17";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_17_1 = {
     pname = "fabric-api-1_17_1";
@@ -208,8 +208,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.46.1+1.17/fabric-api-0.46.1+1.17.jar";
       sha256 = "sha256-XDNYpyTRu2bCHwKsKdY7ZlZBtVam+YIkDQBUdCf0Uv4=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.17.1";
+    modId = "P7dR8mSH";
   };
   fabric-api-1_18 = {
     pname = "fabric-api-1_18";
@@ -218,8 +218,8 @@
       url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/0.44.0+1.18/fabric-api-0.44.0+1.18.jar";
       sha256 = "sha256-ZCByRV30DF6J3XG7wAL48E4uQZ2BvY80ImocIOuRlVY=";
     };
-    modId = "P7dR8mSH";
     mcVer = "1.18";
+    modId = "P7dR8mSH";
   };
   ferritecore-1_16_5 = {
     pname = "ferritecore-1_16_5";
@@ -228,8 +228,8 @@
       url = "https://cdn.modrinth.com/data/uXXizFIs/versions/2.1.0/ferritecore-2.1.0-fabric.jar";
       sha256 = "sha256-FyTpDH5lFOKYwQy6Ynn/4ddhCxU8clGHBrDndUs3KXE=";
     };
-    modId = "uXXizFIs";
     mcVer = "1.16.5";
+    modId = "uXXizFIs";
   };
   ferritecore-1_17 = {
     pname = "ferritecore-1_17";
@@ -238,8 +238,8 @@
       url = "https://cdn.modrinth.com/data/uXXizFIs/versions/3.0.1/ferritecore-3.0.1-fabric.jar";
       sha256 = "sha256-wqk2B+dcAupiZ73IfBPHxBXxCZw7x3gfhB0pqsq6uYo=";
     };
-    modId = "uXXizFIs";
     mcVer = "1.17";
+    modId = "uXXizFIs";
   };
   ferritecore-1_17_1 = {
     pname = "ferritecore-1_17_1";
@@ -248,8 +248,8 @@
       url = "https://cdn.modrinth.com/data/uXXizFIs/versions/3.1.0/ferritecore-3.1.0-fabric.jar";
       sha256 = "sha256-DZdL1vkQBYaaVQokws5UOMe/oZmnJnK/SnCcCKMDRzs=";
     };
-    modId = "uXXizFIs";
     mcVer = "1.17.1";
+    modId = "uXXizFIs";
   };
   ferritecore-1_18 = {
     pname = "ferritecore-1_18";
@@ -258,8 +258,8 @@
       url = "https://cdn.modrinth.com/data/uXXizFIs/versions/4.0.0/ferritecore-4.0.0-fabric.jar";
       sha256 = "sha256-YK0d0+tZWybchXH7hhCG+DV1268K+rpPATQBTK6DBMo=";
     };
-    modId = "uXXizFIs";
     mcVer = "1.18";
+    modId = "uXXizFIs";
   };
   krypton-1_16_5 = {
     pname = "krypton-1_16_5";
@@ -268,8 +268,8 @@
       url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/0.1.2/krypton-0.1.2.jar";
       sha256 = "sha256-U/i8qX6qtznjjtDaC8fTBT8RQ9sozwfVjHLMlUVrB5g=";
     };
-    modId = "fQEb0iXm";
     mcVer = "1.16.5";
+    modId = "fQEb0iXm";
   };
   krypton-1_17 = {
     pname = "krypton-1_17";
@@ -278,8 +278,8 @@
       url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/0.1.3/krypton-0.1.3.jar";
       sha256 = "sha256-BLi6bGzWVaeMzoFNZXSj8zPUGsxIgb2wo05oIkzI4kU=";
     };
-    modId = "fQEb0iXm";
     mcVer = "1.17";
+    modId = "fQEb0iXm";
   };
   krypton-1_17_1 = {
     pname = "krypton-1_17_1";
@@ -288,8 +288,8 @@
       url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/0.1.4/krypton-0.1.4.jar";
       sha256 = "sha256-7FLzkVjDteRAAc+sNHaVLE5AH+jUdJYcrm9EFBwWcLY=";
     };
-    modId = "fQEb0iXm";
     mcVer = "1.17.1";
+    modId = "fQEb0iXm";
   };
   krypton-1_18_1 = {
     pname = "krypton-1_18_1";
@@ -298,8 +298,8 @@
       url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/0.1.9/krypton-0.1.9.jar";
       sha256 = "sha256-sjZr1claHEPhtFWAihoDfMuel9RJW/ZtorTp2nY/sX4=";
     };
-    modId = "fQEb0iXm";
     mcVer = "1.18.1";
+    modId = "fQEb0iXm";
   };
   lazydfu = {
     pname = "lazydfu";
@@ -316,8 +316,8 @@
       url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/mc1.16.5-0.6.6/lithium-fabric-mc1.16.5-0.6.6.jar";
       sha256 = "sha256-kGe0VQn8MCPXa/+FrQMO8B0w5an7/ngV/2Zq0xu6TSg=";
     };
-    modId = "gvQqBUqZ";
     mcVer = "1.16.5";
+    modId = "gvQqBUqZ";
   };
   lithium-1_17_1 = {
     pname = "lithium-1_17_1";
@@ -326,8 +326,8 @@
       url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/mc1.17.1-0.7.5/lithium-fabric-mc1.17.1-0.7.5.jar";
       sha256 = "sha256-HADtQ51u6ZBqHS3q9wZeOYAz96E2vFPSmL4sxjYoMhA=";
     };
-    modId = "gvQqBUqZ";
     mcVer = "1.17.1";
+    modId = "gvQqBUqZ";
   };
   lithium-1_18 = {
     pname = "lithium-1_18";
@@ -336,8 +336,8 @@
       url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/mc1.18-0.7.6-rc1/lithium-fabric-mc1.18-0.7.6-rc1.jar";
       sha256 = "sha256-dggI0EBWNh8r20zqmEwtAcKrgQwhhhPqtbWBdOcdt2c=";
     };
-    modId = "gvQqBUqZ";
     mcVer = "1.18";
+    modId = "gvQqBUqZ";
   };
   lithium-1_18_1 = {
     pname = "lithium-1_18_1";
@@ -346,8 +346,8 @@
       url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/mc1.18.1-0.7.8/lithium-fabric-mc1.18.1-0.7.8.jar";
       sha256 = "sha256-HMwm2u5CY1rHKV1MDl14uoNh4gPOoE1GeJ9zqBo5dCk=";
     };
-    modId = "gvQqBUqZ";
     mcVer = "1.18.1";
+    modId = "gvQqBUqZ";
   };
   starlight-fabric-1_17 = {
     pname = "starlight-fabric-1_17";
@@ -356,8 +356,8 @@
       url = "https://cdn.modrinth.com/data/H8CaAYZC/versions/Starlight%201.0.0%201.17.x/starlight-1.0.0+fabric.73f6d37.jar";
       sha256 = "sha256-oLJ282+JieNpThs2ULghn9KTSTYDXlY8cZ4Y4niC2Ow=";
     };
-    modId = "H8CaAYZC";
     mcVer = "1.17";
+    modId = "H8CaAYZC";
   };
   starlight-fabric-1_18 = {
     pname = "starlight-fabric-1_18";
@@ -366,8 +366,8 @@
       url = "https://cdn.modrinth.com/data/H8CaAYZC/versions/Starlight%201.0.0%201.18.x/starlight-1.0.0+fabric.d0a3220.jar";
       sha256 = "sha256-QdFnvpR5VWNRvxh1h8/C5QFds4BT0VO6sYhBWEiNaS8=";
     };
-    modId = "H8CaAYZC";
     mcVer = "1.18";
+    modId = "H8CaAYZC";
   };
   starlight-forge-1_17_1 = {
     pname = "starlight-forge-1_17_1";
@@ -376,8 +376,8 @@
       url = "https://cdn.modrinth.com/data/iRfIGC1s/versions/Starlight%201.0.0%201.17.x/starlight-1.0.0+forge.5d16647.jar";
       sha256 = "sha256-7pPXm47HUCpfBMRpg5D4HmRl2lM7PpQtsWmLL9i91Yw=";
     };
-    modId = "iRfIGC1s";
     mcVer = "1.17.1";
+    modId = "iRfIGC1s";
   };
   starlight-forge-1_18_1 = {
     pname = "starlight-forge-1_18_1";
@@ -386,7 +386,7 @@
       url = "https://cdn.modrinth.com/data/iRfIGC1s/versions/1.0.1+1.18.1/starlight-1.0.1+forge.fef9eb9.jar";
       sha256 = "sha256-Xn/Na53E7EK14Z26Q6ifCPViljAHlHm72hwS9U3dimk=";
     };
-    modId = "iRfIGC1s";
     mcVer = "1.18.1";
+    modId = "iRfIGC1s";
   };
 }
