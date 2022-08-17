8de959eb5f6ace8af677a97fdc11e386211c6f0d
Daniel Phan <danielphan.2003@c-137.me>
2022-08-17 09:36:51 +0700

    gum-fmt: add warn, fg_yellow, fixup for fg_red

--- cells/fog/cli/utils/gum-fmt.bash
+++ cells/fog/cli/utils/gum-fmt.bash
@@ -1,13 +1,17 @@
-function fg_green() {
-  echo -n '{{ Foreground "#41a25e" "'"$@"'" }}'
-}
-
 function fg_blue() {
   echo -n '{{ Foreground "#4688c4" "'"$@"'" }}'
 }
 
+function fg_green() {
+  echo -n '{{ Foreground "#41a25e" "'"$@"'" }}'
+}
+
 function fg_red() {
-  echo -n '{{ Foreground "#ff0000" "Red Foreground" }}'
+  echo -n '{{ Foreground "#ff0000" "'"$@"'" }}'
+}
+
+function fg_yellow() {
+  echo -n '{{ Foreground "#ffff00" "'"$@"'" }}'
 }
 
 function bold() {
@@ -18,10 +22,14 @@ function debug_type() {
   echo -n "$1: $2: ${@: 3}" | gum format -t template
 }
 
+function error() {
+  debug_type "$(fg_red "error")" "$1" "${@: 2}"
+}
+
 function trace() {
   debug_type "$(fg_blue "trace")" "$1" "${@: 2}"
 }
 
-function error() {
-  debug_type "$(fg_red "error")" "$1" "${@: 2}"
+function warn() {
+  debug_type "$(fg_yellow "warn")" "$1" "${@: 2}"
 }
